36e50fb0781b17f825953a26eed6d02650f8d9b0
JulijaReplai <filipovicjuls@gmail.com>
2026-01-20 19:42:38 +0100

    vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Outfit, Space_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const outfit = Outfit({ subsets: ["latin"], weight: ["400", "700"] });
 const spaceMono = Space_Mono({ subsets: ["latin"], weight: ["400", "700"], variable: "--font-mono" });
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
     <html lang="en" className="scroll-smooth">
       <body className={`${outfit.className} ${spaceMono.variable} bg-slate-950 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
